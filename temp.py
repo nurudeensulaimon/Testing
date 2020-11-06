aeafa03392663bfee88914a95683f8fea74e7e22
Nurudeen Sulaimon <nurudeen.a.sulaimon@gmail.com>
2020-11-06 07:22:41 -0500

    Added a new function

---
+++ temp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def addition(a,b):
+    '''Add two Numbers Together.'''
+    return a + b 
+
+test=addition(12,13)
+print(test)
